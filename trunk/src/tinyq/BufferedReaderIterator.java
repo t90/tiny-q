d116693f384f5c602f9ae11c89221cb97c4ff7cc
vasiltsov@gmail.com <vasiltsov@gmail.com@fdd1015e-e19c-dfaa-5b67-cbef2d0bcc47>
2011-11-25 01:29:58 +0000

    BufferedReaderIterator is now a separate compilation uint

---
+++ trunk/src/tinyq/BufferedReaderIterator.java
@@ -0,0 +1,64 @@
+package tinyq;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class BufferedReaderIterator<T extends String> implements Iterator<String> {
+    private BufferedReader _reader;
+
+    public BufferedReaderIterator(BufferedReader reader) {
+        _reader = reader;
+    }
+
+    String _str = null;
+    boolean _hasCached = false;
+
+
+    public boolean hasNext() {
+        if (_hasCached) {
+            return true;
+        }
+        try {
+            _str = _reader.readLine();
+            if (_str != null) {
+                _hasCached = true;
+            }
+        } catch (IOException e) {
+            StringBuilder sb = new StringBuilder();
+            String var = e.getMessage();
+            if (var != null) {
+                sb.append(var);
+                sb.append("\n");
+            }
+
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            if (stackTrace != null) {
+                Query<String> query = (new Query<StackTraceElement>(stackTrace)).select(new Query.Func<StackTraceElement, String>() {
+                    public String run(StackTraceElement in) {
+                        return in.toString();
+                    }
+                });
+                sb.append(query.aggregate(new StringBuilder(), new Query.Accum<Query<String>, StringBuilder>() {
+                    public StringBuilder run(Query<String> in, StringBuilder in2) {
+                        in2.append(in);
+                        return in2;
+                    }
+                }).toString());
+            }
+            throw new IndexOutOfBoundsException(sb.toString());
+        }
+        return false;
+    }
+
+    public String next() {
+        if (hasNext()) {
+            return _str;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void remove() {
+        throw new IndexOutOfBoundsException("Operation not supported");
+    }
+}
